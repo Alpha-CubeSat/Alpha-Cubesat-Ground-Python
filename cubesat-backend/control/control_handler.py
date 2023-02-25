84208ee42b27e37f8f9bb52f69f5af8e72ccd2c8
Eric <ewz4@cornell.edu>
2023-02-25 11:41:31 -0500

    Added command logging

--- cubesat-backend/control/control_handler.py
+++ cubesat-backend/control/control_handler.py
@@ -1,10 +1,32 @@
+from datetime import datetime
 
-"Stores the result of submitting a command into ElasticSearch"
-def log_command(imei, raw_request, parsed_string, rockblock_response):
-    pass
+"Stores the result of uplinking a command into the command text file logs."
+def log_command(command):
+    currentMinute = datetime.now().minute
+    currentHour = datetime.now().hour
+    currentDay = datetime.now().day
+    currentMonth = datetime.now().month
+    currentYear = datetime.now().year
 
-"""Handles a command sent to ground system for the satellite.
-Authenticates with rockblock web services using credentials
+    time = str(currentHour) + ":" + str(currentMinute)
+    date = str(currentMonth) + "/" + str(currentDay) + "/" + str(currentYear)
+    writeContent = "\n" + command + ", " + time + ", " + date
+    filename = "commands_" + currentYear + ".txt"
+    file = open(filename, 'a')
+    file.write(writeContent)
+    file.close()
+
+    # TODO: Add RockBlock response and status of the command
+
+"""Handles a list of commands created by a ground system to send to
+the satellite. Authenticates with rockblock web services using credentials
 provided in the config file"""
-def handle_command(command):
-    pass
+def handle_command(commands):
+    print("Processing commands!")
+    for command in commands:
+        log_command(command)
+
+    # TODO: Add command parsing and Rockblock connection after getting complete
+    # list of new commands and new opcodes, any changes to the command format,
+    # and setting up the Rockblock to send and receive messages on the portal
+
