d42a993121426c13b67d9d08a8ab821b833348f8
Jonathan Ma <jonathanjma03@gmail.com>
2023-09-01 12:06:42 -0400

    Add networking for admin user management

--- cubesat-frontend/src/pages/AdminUserManage.js
+++ cubesat-frontend/src/pages/AdminUserManage.js
@@ -1,41 +1,49 @@
 import {
   Button,
-  Modal,
-  Container,
-  Col,
-  Row,
   Card,
   CloseButton,
+  Col,
+  Container,
   Form,
+  Modal,
+  Row,
+  Spinner,
 } from "react-bootstrap";
 import InputField from "../components/InputField";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { useApi } from "../contexts/ApiProvider";
 
 // User Management Modal (For admin user only)
 // Modal to allow the admin to create usernames + passwords for new users
 // as well as deleting current users
 export default function AdminUserManage({ show, setShow }) {
-  const usernameField = useRef();
-  const passwordField = useRef();
-  const commandPermField = useRef();
+  const usernameField = useRef("");
+  const passwordField = useRef("");
+  // const commandPermField = useRef();
   const [formErrors, setFormErrors] = useState({});
-  const [allUsers, setAllUsers] = useState([
-    "Admin",
-    "Josh",
-    "Jonathan",
-    "Eric",
-  ]);
 
-  // keeps track of which users are added and deleted:
-  // added users mapped to password while deleted users mapped to null
-  const [statusMap, setStatusMap] = useState({});
+  const [allUsers, setAllUsers] = useState();
+
+  const api = useApi();
+
+  // load list of users with modal opened
+  useEffect(() => {
+    (async () => {
+      const response = await api.get("/user/list");
+      if (response.status === 200) {
+        setAllUsers(response.data["users"]);
+      } else {
+        setAllUsers([]);
+      }
+    })();
+  }, [api]);
 
   const handleClose = () => {
-    setStatusMap({});
     setShow(false);
   };
 
-  const onAddSubmit = (ev) => {
+  // validates and makes API request to create new GSW users
+  const onAddSubmit = async (ev) => {
     ev.preventDefault();
     let username = usernameField.current.value;
     let password = passwordField.current.value;
@@ -51,30 +59,37 @@ export default function AdminUserManage({ show, setShow }) {
 
     // enforces password requirements: 8+ chars, 1+ lowercase letter, 1+ uppercase letter,
     // 1+ number, and 1+ special char
-    let pwd_regex = new RegExp(
-      "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*?[_#?!@$%^&*-])(?=.{8,})"
-    );
-    if (!password || !pwd_regex.test(password)) {
-      errors.password =
-        "Password must have ≥ 8 chars and ≥ 1 of each: uppercase letters, lowercase letters, numbers, and special chars.";
+    // let pwd_regex = new RegExp(
+    //     "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*?[_#?!@$%^&*-])(?=.{8,})"
+    // );
+    // if (!password || !pwd_regex.test(password)) {
+    //   errors.password =
+    //       "Password must have ≥ 8 chars and ≥ 1 of each: uppercase letters, lowercase letters, numbers, and special chars.";
+    // }
+
+    // temporary, enable restrictions when deployed
+    if (!password) {
+      errors.password = "Password must not be empty";
     }
     setFormErrors(errors);
     if (Object.keys(errors).length > 0) return;
 
-    setAllUsers([...allUsers, username]);
-    setStatusMap({ ...statusMap, [username]: password });
+    const response = await api.post("/user/", {
+      username: username,
+      password: password,
+    });
+    if (response.status === 200) {
+      setAllUsers([...allUsers, username]);
+    }
   };
 
-  const removeUser = (username) => {
-    if (username === "Admin") return; // disallow deleting admin user
-    setAllUsers(allUsers.filter((user) => user !== username));
-    setStatusMap({ ...statusMap, [username]: null });
-  };
-
-  const saveChanges = () => {
-    handleClose();
-
-    // api logic
+  // makes API request to remove GSW users
+  const removeUser = async (username) => {
+    if (username === "admin") return; // disallow deleting admin user
+    const response = await api.delete("/user/?username=" + username);
+    if (response.status === 204) {
+      setAllUsers(allUsers.filter((user) => user !== username));
+    }
   };
 
   return (
@@ -108,14 +123,14 @@ export default function AdminUserManage({ show, setShow }) {
                   placeholder="Enter a password"
                   error={formErrors.password}
                   fieldRef={passwordField}
-                  className="mb-2"
+                  className="mb-3"
                 />
-                <Form.Check
-                  type="checkbox"
-                  label="Can Send Commands"
-                  ref={commandPermField}
-                  className="mb-2 ms-1"
-                />
+                {/*<Form.Check*/}
+                {/*  type="checkbox"*/}
+                {/*  label="Can Send Commands"*/}
+                {/*  ref={commandPermField}*/}
+                {/*  className="mb-2 ms-1"*/}
+                {/*/>*/}
                 <Button variant="secondary" type="submit">
                   + Add
                 </Button>
@@ -124,30 +139,28 @@ export default function AdminUserManage({ show, setShow }) {
             {/* List of current users with remove buttons */}
             <Col>
               <h5>Current Users</h5>
-              {allUsers.map((user, i) => (
-                <Card key={i} className="m-2">
-                  <Card.Body className="pt-2 pb-2">
-                    <div className="d-flex">
-                      <p className="flex-grow-1 m-0">{user}</p>
-                      {user !== "Admin" && (
-                        <CloseButton onClick={() => removeUser(user)} />
-                      )}
-                    </div>
-                  </Card.Body>
-                </Card>
-              ))}
+              {allUsers === undefined ? (
+                <Spinner animation="border" />
+              ) : (
+                <>
+                  {allUsers.map((user, i) => (
+                    <Card key={i} className="m-2">
+                      <Card.Body className="pt-2 pb-2">
+                        <div className="d-flex">
+                          <p className="flex-grow-1 m-0">{user}</p>
+                          {user !== "admin" && (
+                            <CloseButton onClick={() => removeUser(user)} />
+                          )}
+                        </div>
+                      </Card.Body>
+                    </Card>
+                  ))}
+                </>
+              )}
             </Col>
           </Row>
         </Container>
       </Modal.Body>
-      <Modal.Footer>
-        <Button variant="success" onClick={saveChanges}>
-          Save Changes
-        </Button>
-        <Button variant="secondary" onClick={handleClose}>
-          Cancel
-        </Button>
-      </Modal.Footer>
     </Modal>
   );
 }
