ce632831f1b4a974af34f94ee84aa0b55b674aad
Jonathan Ma <jonathanjma03@gmail.com>
2023-09-23 16:27:35 -0400

    Update sfr override map

--- cubesat-frontend/src/components/CommandLog.js
+++ cubesat-frontend/src/components/CommandLog.js
@@ -1,9 +1,8 @@
 import { Table } from "react-bootstrap";
 import { useDashboard } from "../contexts/DashboardProvider";
-import { useEffect, useCallback } from "react";
+import { useCallback, useEffect } from "react";
 import LogRow from "./LogRow";
 import { useApi } from "../contexts/ApiProvider";
-
 
 // Command History Log
 // Shows a log of all previously sent commands to the CubeSat with each command's status, name,
@@ -13,19 +12,26 @@ export default function CommandLog() {
 
   const api = useApi();
   const checkProcessed = useCallback(async () => {
-    await api.get('/cubesat/commandLog').then(response => {
+    await api.get("/cubesat/processed_commands").then((response) => {
       const dataList = response["data"][response["data"].length - 1];
       const sfrList = [];
       for (let item of dataList) {
         if (item.includes(":")) {
-          sfrList.push(item.substr(0, item.indexOf(" :")), item.substr(item.indexOf(":") + 2));
+          sfrList.push(
+            item.substr(0, item.indexOf(" :")),
+            item.substr(item.indexOf(":") + 2)
+          );
         }
       }
       const newCommandLog = JSON.parse(JSON.stringify(commandLog));
       for (let logEntry of newCommandLog) {
         for (let command of logEntry.commands) {
-          if (!("processed" in command) && (dataList.includes(command["opcode"].toLowerCase()) ||
-            (sfrList.includes(command["namespace"]) && sfrList.includes(command["field"])))) {
+          if (
+            !("processed" in command) &&
+            (dataList.includes(command["opcode"].toLowerCase()) ||
+              (sfrList.includes(command["namespace"]) &&
+                sfrList.includes(command["field"])))
+          ) {
             command["processed"] = "true";
           }
         }
