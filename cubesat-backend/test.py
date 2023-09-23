1eb2198b56819a16cd2ad185e3edc1c7262f6220
Sean Zhang <swz9@cornell.edu>
2023-09-23 14:55:49 -0400

    Time change to test time stamp

--- cubesat-backend/test.py
+++ cubesat-backend/test.py
@@ -36,7 +36,7 @@ rockblock_report4 = {'device_type': "ROCKBLOCK", 'iss': "Rock 7", 'iridium_longi
 rockblock_report5 = {'device_type': "ROCKBLOCK", 'iss': "Rock 7", 'iridium_longitude': -76.6932,
                      'imei': 300534061384690,
                      'serial': 203136, 'iridium_latitude': 39.1125, 'momsn': 34, 'iridium_cep': 3.0,
-                     'transmit_time': "2021-09-12T00:08:34Z",
+                     'transmit_time': "2023-09-12T00:08:34Z",
                      'JWT': "eyJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJsb2NrIDciLCJpYXQiOjE2Mjg0Njc3MTgsImRhdGEiOiIxNTAxMDAwMzAwMDBmNDAwMDAwMDBhMDAwMGZmZmYwMGZmZmZmZmZmMDEwZTAwMDIwMDAwMDEwMTAxMDEwMTAxMDEwMTAxMDAwMTAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMCIsImRldmljZV90eXBlIjoiUk9DS0JMT0NLIiwiaW1laSI6IjMwMDUzNDA2MTM4NDY5MCIsImlyaWRpdW1fY2VwIjoiMy4wIiwiaXJpZGllbV9sYXRpdHVkZSI6IjM5LjExMjUiLCJpcmlkaXVtX2xvbmdpdHVkZSI6Ii03Ni42OTMyIiwiaXJpZGl1bV9zZXNzaW9uX3N0YXR1cyI6IjAiLCJtb21zbiI6IjM0Iiwic2VyaWFsIjoiMjAzMTM2IiwidHJhbnNtaXRfdGltZSI6IjIxLTA4LTA5IDAwOjA4OjM0in0.dUpRBK1dpvyoKQKURa8Ny9dZBB8iZGq0tX4F8VcaTCLvnpjrxjHebMbIKKLK0FbCBCJ1cr_C5DVEsQWV-BXWFXiUriJ_x5ElumyQ6t0tkfhPcC7e4Rveb9fD6OBtbD31XoDbjBUSPLlYWvOWuGI4MDzHMgNoiKDbaUjakhp30uELuVF1OeJWErRTXFpuZEkJzWc9MWKZ04GA85WgjwZ1PsPYONuRODh7itK8VGFzuIAyd8YcvHlkKCWw0y_3m4txZ_FrpM9eBQPwVXITHta7afDgfd-CURjQqjJpCl5oU5cJ9QoBO1K4PJ689oem9_Sbo3j4djdntMy63-XWQHVvvg",
                      'iat': 1628467718, 'iridium_session_status': 0,
                      'data': "631FFF00000000000000000000008123270505052323235F5F261B1500000000000000000D30000000000000000000001901000000000000000000000000000000000000"
