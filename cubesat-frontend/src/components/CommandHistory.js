29cc4ab661704ad18de43e46cd969cf1fad6f02b
Jonathan Ma <jonathanjma03@gmail.com>
2023-10-15 11:14:57 -0400

    reduce autorefresh frequency

--- cubesat-frontend/src/components/CommandHistory.js
+++ cubesat-frontend/src/components/CommandHistory.js
@@ -13,54 +13,53 @@ export default function CommandHistory() {
   const api = useApi();
 
   function isNested(subArr, parentArr) {
-    return parentArr.some(innerArr =>
-      subArr.length === innerArr.length &&
-      subArr.every((elem, idx) => elem === innerArr[idx])
+    return parentArr.some(
+      (innerArr) =>
+        subArr.length === innerArr.length &&
+        subArr.every((elem, idx) => elem === innerArr[idx])
     );
   }
 
   const checkProcessed = useCallback(async () => {
-    console.log("fetching processed opcodes")
+    console.log("fetching processed opcodes");
     await api.get("/cubesat/processed_commands").then((response) => {
       const dataList = response["data"];
       const sfrList = [];
-      const opcodeList = []
+      const opcodeList = [];
       for (let data in dataList) {
         for (let item of dataList[data]) {
           if (item.includes("::")) {
-            sfrList.push(
-              [item.substr(0, item.indexOf(":")),
-              item.substr(item.indexOf(":") + 2)]
-            );
-          }
-          else {
-            opcodeList.push(item)
+            sfrList.push([
+              item.substr(0, item.indexOf(":")),
+              item.substr(item.indexOf(":") + 2),
+            ]);
+          } else {
+            opcodeList.push(item);
           }
         }
       }
       const newCommandLog = JSON.parse(JSON.stringify(commandLog));
       for (let i = newCommandLog.length - 1; i >= 0; i--) {
-        let logEntry = newCommandLog[i]
+        let logEntry = newCommandLog[i];
         for (let command of logEntry.commands) {
           if (opcodeList.includes(command["opcode"])) {
-            command["processed"] = "true"
-            setCommandLog(newCommandLog);
-            let index = opcodeList.indexOf(command["opcode"])
-            opcodeList.splice(index, 1);
-          }
-          else if
-            (isNested([command["namespace"], command["field"]], sfrList)) {
             command["processed"] = "true";
-            let index = sfrList.indexOf(command["namespace"])
+            setCommandLog(newCommandLog);
+            let index = opcodeList.indexOf(command["opcode"]);
+            opcodeList.splice(index, 1);
+          } else if (
+            isNested([command["namespace"], command["field"]], sfrList)
+          ) {
+            command["processed"] = "true";
+            let index = sfrList.indexOf(command["namespace"]);
             sfrList.splice(index, 1);
-            let index2 = sfrList.indexOf(command["field"])
+            let index2 = sfrList.indexOf(command["field"]);
             sfrList.splice(index2, 1);
             setCommandLog(newCommandLog);
           }
         }
       }
-    }
-    );
+    });
   }, [api, commandLog, setCommandLog]);
 
   // Checks whether a command has appeared in the command log of the downlinked normal report
@@ -68,7 +67,7 @@ export default function CommandHistory() {
     // Poll every 10000 milliseconds (10 seconds)
     const interval = setInterval(() => {
       checkProcessed();
-    }, 5000);
+    }, 10000);
 
     // Cleanup: clear the interval when the component is unmounted or the effect re - runs
     return () => {
